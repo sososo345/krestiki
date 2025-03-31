ecba655703c9cf1a66c65d16ab7d2f788ce00833
sososo345 <zh.gadzevich@mail.ru>
2025-03-31 16:30:12 +0300

    Add files via upload

--- krest.py
+++ krest.py
@@ -1,24 +1,50 @@
 import random
 
 game = [
-    ['{ }','{ }','{ }'],
-    ['{ }','{ }','{ }'],
-    ['{ }','{ }','{ }']
+    ['','',''],
+    ['','',''],
+    ['','','']
 ]
-def main():
-    flag=True
-    move=0
-    while flag:
-        x,y = input()
-        if move%2==0:
-            game,move = def3 (game"x",x,y,move)
-        else:
-            game,move = def3 (game"x",x,y,move)
-        flag = def2(game)
-        move +=1
-def moving ():
+def movingsucces(game,s,x,y):
     if game[x][y]=="":
         game[x][y]= s
     else:
         move-=1
     return game,move
+def moved(move):
+    for i in range(3):
+        if game[i][0]==  game[i][1]== game[i][2]!="":
+            print ("win")
+            return False   
+        elif game[0][i]==  game[1][i]== game[2][i]!="":
+            print("win")
+            return False
+        else:
+            return True
+    if game[0][0]==game[1][1]==game[2][2]=='':
+        print('win')
+        return False
+    else:
+        return True
+    if move>=9:
+        print('nikto ne veigral')
+        return False
+def main(game):
+    flag=True
+    move=0
+    while flag:
+        x = int(input())
+        y = int(input())
+        if move%2==0:
+            game,move = movingsucces(game,"x",x,y,move)
+        else:
+            game,move = movingsucces(game,"0",x,y,move)
+        flag = moved(game)
+        move +=1
+main(game)
+
+        
+
+            
+
+    
