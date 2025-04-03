4a16f94e4c2e154e02c4f75b01ab3323f2411829
sososo345 <zh.gadzevich@mail.ru>
2025-04-03 16:24:54 +0300

    Add files via upload

--- krest.py
+++ krest.py
@@ -21,26 +21,45 @@ def moved(move):
             return False
         else:
             return True
+    if move>=9:
+        print('nikto ne veigral')
+        return False
     if game[0][0]==game[1][1]==game[2][2]=='':
         print('win')
         return False
     else:
         return True
-    if move>=9:
-        print('nikto ne veigral')
-        return False
+    
+def bot():
+    movebot=True
+    while movebot:
+        botx = random.choose(0,1,2)
+        boty = random.choose(0,1,2)
+        movebot = main(game)
+        return botx, boty
+
+
+
+
+
 def main(game):
     flag=True
     move=0
     while flag:
         x = int(input())
         y = int(input())
-        if move%2==0:
-            game,move = movingsucces(game,"x",x,y,move)
-        else:
-            game,move = movingsucces(game,"0",x,y,move)
         flag = moved(game)
         move +=1
+        if move%2==0:
+            game,move = movingsucces(game,"x",x,y,move)
+            return True
+        else:
+            botx, boty = bot()
+            game,move = movingsucces(game,"0",botx,boty,move)
+            return False
+            
+        
+
 main(game)
 
         
